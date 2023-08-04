f9607eccd59f013d8adebd0a83eb78f42b0b4160
Vinayak-Mali-03 <127152926+Vinayak-Mali-03@users.noreply.github.com>
2023-08-05 01:55:33 +0530

    Create Hello_World
    This source code will show the "Hello World!" on the output window. In this code only declarations  of the functions are used on the top of the code. 
    It shows the header files are contain a declarations of the function and not the definition of the functions.

---
+++ Language Programs/Hello_World.c
@@ -0,0 +1,20 @@
+/**
+ * @file Hello_World.c
+ * @author Vinayak Mali
+ * @brief To show the Hello World on the output screen
+ * @version 0.1
+ * @date 2023-08-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+int printf(const char* fmt, ...);
+void exit(int);
+
+int main(void)
+{
+    printf("Hello World!");
+
+    exit(0);
+}
