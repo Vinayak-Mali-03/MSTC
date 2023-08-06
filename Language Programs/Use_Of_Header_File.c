ace40519890a101550fdc05f94c9287d477a64d7
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-08-06 17:40:54 +0530

    Initial commit

---
+++ Language Programs/Use_Of_Header_File.c
@@ -0,0 +1,20 @@
+/**
+ * @file Hello_World_With_Header_File.c
+ * @author Vinayak Mali
+ * @brief To show the Hello World on the output window using header file.
+ * @version 0.1
+ * @date 2023-08-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello World!");
+    
+    exit(0);
+}
