a1a7da8823c45586a2b9eaa6fa30182d96600009
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-08-26 23:13:37 +0530

    Initial commit

---
+++ Pointer Programs/022_Offset_Of_Macro.c
@@ -0,0 +1,34 @@
+/**
+ * @file 022_Offset_Of_Macro.c
+ * @author Vinayak Mali
+ * @brief To demonstrate the offset of macro
+ * @version 0.1
+ * @date 2023-08-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef OFFSET_OF
+#define OFFSET_OF(type, mem) ((unsigned long long int)(&((type*)0) -> mem))
+#endif
+
+struct A
+{
+    int a;
+    char b;
+    float c;
+};
+
+int main(void)
+{
+    unsigned long long int off_a, off_b, off_c;
+
+    off_a = OFFSET_OF(struct A, a);
+    off_b = OFFSET_OF(struct A, b);
+    off_c = OFFSET_OF(struct A, c);
+
+    printf("off_a = %llu, off_b = %llu, off_c = %llu\n", off_a, off_b, off_c);
+
+    return (0);
+}
